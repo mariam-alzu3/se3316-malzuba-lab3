d0416777ea75dfb0cf40225ba0772bdfebff9b86
Mariam <mariam.alzu3@gmail.com>
2022-10-15 17:18:24 -0400

    removed duplication - router

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const fs = require('fs');
+const router = express.Router();
+//const csv = require('csv-parser');
 
 //serving front-end code
 app.use('/', express.static('static'));
 
 
+const data = [
+    {genre_id: 1, name: "pop"},
+    {genre_id: 2, name: "rock"}
+];
 
-//const data = [];
-const fs = require('fs');
-//const csv = require('csv-parser');
+// middleware to do logging
+app.use((req, res, next) => {
+    console.log(`${req.method} request for ${req.url}`);
+    next();
+});
 
-
-
-app.get('/api/data', (req, res) => {
+// get list of data
+router.get('/', (req, res) => {
     res.send(data);
 });
 
-app.get('/api/data/:genre_id', (req, res) => {
-    const genre = data.find(c => c.genre_id === (req.params.genre_id));
+// data based on id
+router.get('/:genre_id', (req, res) => {
+    const genre = data.find(c => c.genre_id === parseInt(req.params.genre_id));
     res.send(genre);
     console.log(genre)
 });
 
+app.use('/api/data', router)
 
 app.listen(3000, () => console.log('Listening on port 3000...'))
 
