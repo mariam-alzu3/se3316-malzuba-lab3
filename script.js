7188bcbec263245c56643386991dba9ec36166af
Mariam <mariam.alzu3@gmail.com>
2022-10-15 05:43:58 -0400

    added a csv parser

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const app = express();
+const path = require("path");
+
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get("/javascript", (req, res) => {
+    res.sendFile(path.join(__dirname, "/script.js"))
+});
+
+app.get("/css", (req, res) => {
+    res.sendFile(path.join(__dirname, "/stylesheet.css"))
+});
+
+const data = [];
+const fs = require('fs');
+const csv = require('csv-parser');
+
+
+fs.createReadStream('lab3-data/genres.csv')
+    .pipe(csv())
+    .on('data', (rows) => {
+        data.push(rows);
+    }).on('end', () => {
+        //console.log(data);
+    });
+
+app.get('/api/data', (req, res) => {
+    res.send(data);
+});
+
+app.get('/api/data/:genre_id', (req, res) => {
+    const genre = data.find(c => c.genre_id === (req.params.genre_id));
+    res.send(genre);
+    console.log(genre)
 });
 
 
