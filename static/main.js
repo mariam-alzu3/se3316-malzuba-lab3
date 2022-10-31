2845f565c7f955d3125d7d4f5f86dabbbfa7d6b6
Mariam <mariam.alzu3@gmail.com>
2022-10-30 23:34:17 -0400

    added css to the display of songs on the homepage

--- static/main.js
+++ static/main.js
@@ -6,7 +6,7 @@ const table = document.getElementById('table');
 const btn = document.getElementById('seeMoreRecords')
 
 var start = 0;
-var num = 20;
+var num = 22;
 
 function getData() {
     fetch('http://localhost:3000/api/data/tracks')
@@ -15,7 +15,7 @@ function getData() {
             console.log(data)
             tableData.push(data)
             data.slice(start, num).forEach(track => {
-                const item = `<tr><th>` + track.track_id + `</th><th>` + track.track_title + `</th></tr>`
+                const item = `<tr><th>` + track.track_id + `</th><th>` + track.track_title + `</th><th>` + track.album_title + `</th></tr>`
                 table.insertAdjacentHTML("beforeend", item)
 
                 //const title = `<div>` + genre.title + `</div>`
@@ -33,3 +33,21 @@ getData();
 btn.onclick = function () {
     getData();
 }
+
+
+
+// const artistBtn = document.getElementById('search-artist');
+
+// async function loadArtists(searchTerm) {
+//     const URL = `http://localhost:3000/api/data/artist?searchterm3=${searchTerm}`
+//     const res = await fetch(`${URL}`);
+//     const data = await res.json();
+//     console.log(data)
+//     console.log(searchTerm)
+//     const item = `<div>` + data.searchTerm + `</div>`
+//     main.insertAdjacentHTML("beforeend", item);
+// }
+
+// artistBtn.onclick = function () {
+//     loadArtists();
+// }
