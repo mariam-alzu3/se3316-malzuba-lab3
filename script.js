024057a7e5534ffa725f01524ba5355e4fcc4ad1
Mariam <mariam.alzu3@gmail.com>
2022-11-02 19:55:06 -0400

    implemented frontend search for tracks, albums, and artists

--- script.js
+++ script.js
@@ -15,6 +15,20 @@ const genreInfo = [];
 const albumInfo = [];
 const artistInfo = [];
 
+function selectFewerGenreProps(show) {
+    const { genre_id, title, parent } = show;
+    return { genre_id, title, parent };
+}
+
+function selectFewerArtistProps(show) {
+    const { artist_id, artist_name, artist_bio, artist_active_year_begin, artist_active_year_end, artist_handle, artist_url } = show;
+    return { artist_id, artist_name, artist_bio, artist_active_year_begin, artist_active_year_end, artist_handle, artist_url };
+}
+
+function selectFewerTrackProps(show) {
+    const { track_title, track_genres, track_number, album_id, album_title, artist_id, artist_name, tags, track_date_created, track_date_recorded, track_duration } = show;
+    return { track_title, track_genres, track_number, album_id, album_title, artist_id, artist_name, tags, track_date_created, track_date_recorded, track_duration  };
+}
 
 // Turns it to an array of objects
 fs.createReadStream('lab3-data/genres.csv')
@@ -74,49 +88,62 @@ router.get('/artists', (req, res) => {
     res.send(artistInfo);
 });
 
-// app.get('/api/data/songs', (req, res) => {
-//     res.send("ffff");
-// });
-
-app.get('/api/data/genre', (req, res) => {
-    var searchTerm = req.query.searchterm;
-    console.log(searchTerm);
-
-    const genre = genreInfo.find(c => c.title === searchTerm);
-    if (genreInfo) {
-        res.send(genre);
-    } else {
-        res.status(404).send(`${searchTerm} was not found!`);
-    }
-    console.log(genre)
+app.get('/api/data/genre/:id', (req, res) => {
+    var newGenre = genreInfo.map(selectFewerGenreProps)
+    const genre = newGenre.find(g => g.genre_id === (req.params.id))
+    res.send(genre)
 });
 
 
-app.get('/api/data/album', (req, res) => {
-    var searchTerm = req.query.searchterm2;
-    console.log(searchTerm);
+app.get('/api/data/album/:id', (req, res) => {
 
-    const album = albumInfo.find(c => c.album_title === searchTerm);
+    const album = albumInfo.find(c => c.album_title === req.params.id);
     if (albumInfo) {
-        res.send(album);
+        //res.send(album);
+        // res.send(`
+        // <!DOCTYPE html>
+        // <html>
+        //     <head>
+        //        <link rel="stylesheet" href="stylesheet.css">
+        //        <base?>
+        //     </head>
+        //     <body>
+        //         <p>Name: ${album.album_title}</p>
+        //         <p>ID: ${album.album_id}</p>
+        //         <p>Artist: ${album.artist_name}</p>
+        //         <p>Released: ${album.album_date_released}</p>
+        //         <p>Listens: ${album.album_listens}</p>                
+        //         <p>Website: ${album.album_url}</p>
+        //     </body>
+        // </html>
+        // `)
     } else {
-        res.status(404).send(`${searchTerm} was not found!`);
+        res.status(404).send('Album was not found!');
     }
     console.log(album)
 });
 
-app.get('/api/data/artist', (req, res) => {
-    var searchTerm = req.query.searchterm3;
-    console.log(searchTerm);
-
-    const artist = artistInfo.find(c => c.artist_name === searchTerm);
-    if (albumInfo) {
-        //res.send(artist.artist_id);
-        res.send(artist.artist_id + "\n" + artist.artist_name);
+app.get('/api/data/artist/:id', (req, res) => {
+    var newArtist = artistInfo.map(selectFewerArtistProps)
+    const artist = newArtist.find(a => a.artist_id === (req.params.id))
+    if (newArtist) {
+        res.send(artist)
     } else {
-        res.status(404).send(`${searchTerm} was not found!`);
+        res.status(404).send("Given ID was not found!");
     }
     console.log(artist)
+});
+
+app.get('/api/data/track/:name', (req, res) => {
+
+    var newTrack = trackInfo.map(selectFewerTrackProps)
+    const track = newTrack.find(t => t.track_title === (req.params.name))
+    if (newTrack) {
+        res.send(track)
+    } else {
+        res.status(404).send("Given ID was not found!");
+    }
+    console.log(track)
 });
 
 app.use('/api/data', router)
--- static/main.js
+++ static/main.js
@@ -17,9 +17,6 @@ function getData() {
             data.slice(start, num).forEach(track => {
                 const item = `<tr><th>` + track.track_id + `</th><th>` + track.track_title + `</th><th>` + track.album_title + `</th></tr>`
                 table.insertAdjacentHTML("beforeend", item)
-
-                //const title = `<div>` + genre.title + `</div>`
-                //main.insertAdjacentHTML("beforeend", title)
             })
             start = num;
             num += 5;
@@ -34,20 +31,103 @@ btn.onclick = function () {
     getData();
 }
 
+//************************************************************************************************//
+//************************************************************************************************//
 
+function loadArtists() {
+    let searchInput = document.getElementById('search-artist');
+    const list = document.getElementById('list');               //list to store in artists names
+    let results = [];
+    console.log(results)
+    clearList(list);
+    fetch('/api/data/artists')
+        .then(res => res.json())
+        .then(data => {
+            for (i = 0; i < data.length; i++) {
+                value = data[i].artist_name;
+                if (value.toLowerCase().indexOf(searchInput.value.toLowerCase()) > -1) {
+                    results[i] = data[i];
+                }
+            }
+            console.log(results);
 
-// const artistBtn = document.getElementById('search-artist');
+            results.forEach(e => {
+                const item = document.createElement('li')
+                item.classList.add('search-list-item')
+                item.appendChild(document.createTextNode(`${e.artist_name}`));
+                //const artistInfo = document.createTextNode("Name " + e.artist_name)
+                //item.appendChild(artistInfo);
+                list.appendChild(item);
+            });
+        })
+}
 
-// async function loadArtists(searchTerm) {
-//     const URL = `http://localhost:3000/api/data/artist?searchterm3=${searchTerm}`
-//     const res = await fetch(`${URL}`);
-//     const data = await res.json();
-//     console.log(data)
-//     console.log(searchTerm)
-//     const item = `<div>` + data.searchTerm + `</div>`
-//     main.insertAdjacentHTML("beforeend", item);
-// }
+function loadAlbums() {
+    let searchInput = document.getElementById('search-album');
+    const list = document.getElementById('list');               //list to store in artists names
+    let results = [];
+    console.log(results)
+    clearList(list);
+    fetch('/api/data/albums')
+        .then(res => res.json())
+        .then(data => {
+            for (i = 0; i < data.length; i++) {
+                value = data[i].album_title;
+                if (value.toLowerCase().indexOf(searchInput.value.toLowerCase()) > -1) {
+                    results[i] = data[i];
+                }
+            }
+            console.log(results);
 
-// artistBtn.onclick = function () {
-//     loadArtists();
+            results.forEach(e => {
+                const item = document.createElement('li')
+                item.classList.add('search-list-item')
+                item.appendChild(document.createTextNode(`${e.album_title}`));
+                //const artistInfo = document.createTextNode("Name " + e.artist_name)
+                //item.appendChild(artistInfo);
+                list.appendChild(item);
+            });
+        })
+}
+
+function loadTracks() {
+    let searchInput = document.getElementById('search-track');
+    const list = document.getElementById('list');               //list to store in artists names
+    let results = [];
+    console.log(results)
+    clearList(list);
+    fetch('/api/data/tracks')
+        .then(res => res.json())
+        .then(data => {
+            for (i = 0; i < data.length; i++) {
+                value = data[i].track_title;
+                if (value.toLowerCase().indexOf(searchInput.value.toLowerCase()) > -1) {
+                    results[i] = data[i];
+                }
+            }
+            console.log(results);
+
+            results.forEach(e => {
+                const item = document.createElement('li')
+                item.classList.add('search-list-item')
+                item.appendChild(document.createTextNode(`${e.track_title}`));
+                //const artistInfo = document.createTextNode("Name " + e.artist_name)
+                //item.appendChild(artistInfo);
+                list.appendChild(item);
+            });
+        })
+}
+
+function clearList(element) {                                      //clears the list of older searches
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+document.getElementById('search-track-button').addEventListener('click', loadTracks);
+document.getElementById('search-album-button').addEventListener('click', loadAlbums);
+document.getElementById('search-artist-button').addEventListener('click', loadArtists);
+
+// function passValues() {
+//     localStorage.setItem("textvalue", list)
 // }
