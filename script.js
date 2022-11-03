8d8b85bfdf184ed67677f970973cf5a96614d0b3
Mariam <mariam.alzu3@gmail.com>
2022-11-03 05:17:11 -0400

    added delete to remove specific playlists by name

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const genreInfo = [];
 const albumInfo = [];
 const artistInfo = [];
 const playlists = [
-    { id: "1", name: "x", tracks: [ "song 1", "song 2"] }
+    { id: "1", name: "x", tracks: ["song 1", "song 2"] }
 ];
 
 //functions to select specific properties
@@ -112,14 +112,40 @@ router.get('/artists', (req, res) => {
     res.send(artistInfo);
 });
 
-//get genre info by genre ID
+//1. get genre info by genre ID
 app.get('/api/data/genres/:id', (req, res) => {
     var newGenre = genreInfo.map(selectFewerGenreProps)
     const genre = newGenre.find(g => g.genre_id === (req.params.id))
     res.send(genre)
 });
 
-//get album info by album name
+
+//2. get artist info by artist ID
+app.get('/api/data/artists/:id', (req, res) => {
+    var newArtist = artistInfo.map(selectFewerArtistProps)
+    const artist = newArtist.find(a => a.artist_id === (req.params.id))
+    if (newArtist) {
+        res.send(artist)
+    } else {
+        res.status(404).send("Given ID was not found!");
+    }
+    console.log(artist)
+});
+
+
+//3. get track information by track ID
+app.get('/api/data/tracks/:id', (req, res) => {
+    var newTrack = trackInfo.map(selectFewerTrackProps)
+    const track = newTrack.find(t => t.track_id === (req.params.id))
+    if (newTrack) {
+        res.send(track)
+    } else {
+        res.status(404).send("Given ID was not found!");
+    }
+    console.log(track)
+});
+
+//4. get album info by album name
 app.get('/api/data/albums/:name', (req, res) => {
     var newAlbum = albumInfo.map(selectFewerAlbumProps)
     const album = newAlbum.find(al => al.album_title === (req.params.name))
@@ -157,36 +183,12 @@ app.get('/api/data/albums/:name', (req, res) => {
 });
 
 
-//get artist info by artist ID
-app.get('/api/data/artists/:id', (req, res) => {
-    var newArtist = artistInfo.map(selectFewerArtistProps)
-    const artist = newArtist.find(a => a.artist_id === (req.params.id))
-    if (newArtist) {
-        res.send(artist)
-    } else {
-        res.status(404).send("Given ID was not found!");
-    }
-    console.log(artist)
-});
-
-
-//get track information by track ID
-app.get('/api/data/tracks/:id', (req, res) => {
-    var newTrack = trackInfo.map(selectFewerTrackProps)
-    const track = newTrack.find(t => t.track_id === (req.params.id))
-    if (newTrack) {
-        res.send(track)
-    } else {
-        res.status(404).send("Given ID was not found!");
-    }
-    console.log(track)
-});
-
-
-//get tracks IDs from track title
+//4. get tracks IDs from track title
 app.get('/api/data/tracks-title/:name', (req, res) => {
     var newTrack = trackInfo.map(selectFewerTrackTitleProps)
     const track = newTrack.filter(t => t.track_title.toLowerCase() === (req.params.name.toLowerCase())).slice(0, 11)
+    //const {track_title, ...rest} = track;
+
     if (newTrack) {
         res.send(track)
     } else {
@@ -196,7 +198,7 @@ app.get('/api/data/tracks-title/:name', (req, res) => {
 });
 
 
-//get track IDs from album title
+//4. get track IDs from album title
 app.get('/api/data/albums-title/:name', (req, res) => {
     var tracksFromAlbum = trackInfo.map(selectFewerAlbumTracksProps)
     const tracks = tracksFromAlbum.filter(a => a.album_title.toLowerCase() === (req.params.name.toLowerCase())).slice(0, 11)
@@ -209,13 +211,29 @@ app.get('/api/data/albums-title/:name', (req, res) => {
 });
 
 
-//get playlists
-router.get('/playlists', (req, res) => {
-    res.send(playlists);
+//5. artists IDs from artist name
+app.get('/api/data/artist-name/:name', (req, res) => {
+    var newArtistArr = artistInfo.map((data) => {
+        return {
+            'artist_name': data.artist_name,
+            'artist_id': data.artist_id
+        }
+    });
+
+    const artistIDs = newArtistArr.filter(a => a.artist_name.toLowerCase() === req.params.name.toLowerCase());
+
+    if (artistIDs) {
+        // const { artist_name, ...rest } = artistIDs;
+        // res.send(rest)
+        res.send(artistIDs);
+    } else {
+        res.status(404).send(`Artist "${req.params.name}" was not found!`);
+    }
+    console.log(artistIDs);
 });
 
 
-// create new playlist
+//6. create new playlist
 router.put('/playlists/:name', (req, res) => {
     const newPlaylist = req.body;
     console.log("Playlist Name: ", newPlaylist)
@@ -229,9 +247,76 @@ router.put('/playlists/:name', (req, res) => {
         playlists.push(newPlaylist);
     } else {
         console.log("Playlist already exists");
+        res.status(404).send(`Playlist "${req.params.name}" already exists!`);
+    }
+    res.send(newPlaylist);
+});
+
+
+//7. update tracks in a list (doesn't work)
+router.post('/playlists/:name', (req, res) => {
+    const newPlaylist = req.body;
+    console.log("Track Titles: ", newPlaylist)
+
+    //find list
+    const playlist = playlists.find(p => p.name === (req.params.name));
+
+    //replace
+    if (!playlists.includes(playlist)) {
+        res.status(404).send(`List ${req.params.name} not found!`)
+    } else {
+        console.log("Updating tracks for ", req.params.name);
+        //playlists[playlist.tracks] += req.body.tracks
+
+        for (i = 0; i <= playlists.length; i++) {
+            console.log(playlists.length)
+            playlists[playlist.tracks[i]] = req.body.tracks[i]
+
+            console.log(playlist.tracks[i])
+            console.log(req.body.tracks[i])
+        }
+        res.send(req.body)
+    }
+});
+
+
+//8. get tracks IDs from list
+router.get('/playlists/:name', (req, res) => {
+    var newTrackList = playlists.map((data) => {
+        return {
+            'name': data.name,
+            'tracks': data.tracks
+        }
+    });
+
+    if (trackList = newTrackList.find(t => t.name === req.params.name)) {
+        const { name, ...rest } = trackList;
+        res.send(rest)
+        console.log(rest)
+    } else {
+        console.log("not found")
+        res.status(404).send(`Playlist "${req.params.name}" was not found!`);
     }
 
-    res.send(newPlaylist);
+});
+
+//9. delete playlist by name
+router.delete('/playlists/:name', (req, res) => {
+    let listName = req.params.name;
+    let index = playlists.findIndex(i => i.name === listName);
+
+    if (index >= 0) {
+        let deletedList = playlists.splice(index, 1);
+        res.send(deletedList);
+    } else {
+        res.status(404).send(`Playlist "${req.params.name}" does not exist!`);
+    }
+});
+
+
+//10. get playlists (not done)
+router.get('/playlists', (req, res) => {
+    res.send(playlists);
 });
 
 
